dbeb005cedab63f0115dd78d12826b32a54925fe
student <student@gnu.ac.in>
2023-08-07 08:54:30 +0530

    pr4

--- app/src/main/java/com/example/mad_pr4_21012011063/RegistationActivity.kt
+++ app/src/main/java/com/example/mad_pr4_21012011063/RegistationActivity.kt
@@ -3,12 +3,9 @@ package com.example.mad_pr4_21012011063
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegistationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
+        setContentView(R.layout.activity_registation)
     }
 }
